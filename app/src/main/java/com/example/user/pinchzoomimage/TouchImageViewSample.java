53a925baceb93fe7cb9fa207aa89874d5232dc5a
Serhii Kamenkovych <skamenkovych@codeminders.com>
2016-09-19 18:24:58 +0300

    Implemented panning

--- app/src/main/java/com/example/user/pinchzoomimage/TouchImageViewSample.java
+++ app/src/main/java/com/example/user/pinchzoomimage/TouchImageViewSample.java
@@ -2,7 +2,6 @@ package com.example.user.pinchzoomimage;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.graphics.Canvas;
 import android.graphics.Matrix;
 import android.graphics.PointF;
 import android.graphics.RectF;
@@ -22,33 +21,16 @@ public class TouchImageViewSample extends ImageView {
     private static final String TAG = TouchImageViewSample.class.getSimpleName();
     private final float minDistance;
 
-    private float mPosX = 0f;
-    private float mPosY = 0f;
-
-    private float mLastTouchX;
-    private float mLastTouchY;
     private static final int INVALID_POINTER_ID = -1;
 
     // These matrices will be used to move and zoom image
     Matrix matrix = new Matrix();
     Matrix savedMatrix = new Matrix();
-    private static final Matrix IDENTITY = new Matrix();
     private final RectF imageRect = new RectF();
     private final RectF tempRect = new RectF();
-    // We can be in one of these 3 states
-    static final int NONE = 0;
-    static final int DRAG = 1;
-    static final int ZOOM = 2;
-    int mode = NONE;
 
     // Remember some things for zooming
     PointF start = new PointF();
-    PointF mid = new PointF();
-    float oldDist = 1f;
-    String savedItemClicked;
-
-    float pivotPointX = 0f;
-    float pivotPointY = 0f;
 
     // The ‘active pointer’ is the one currently moving our object.
     private int mActivePointerId = INVALID_POINTER_ID;
@@ -75,61 +57,42 @@ public class TouchImageViewSample extends ImageView {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return mScaleDetector.onTouchEvent(event);
-        //if ((event.getAction() & MotionEvent.ACTION_MASK) != MotionEvent.ACTION_MOVE) {
-        //    dumpEvent(event);
-        //}
-        //
-        //// Handle touch events here...
-        //switch (event.getAction() & MotionEvent.ACTION_MASK) {
-        //    case MotionEvent.ACTION_DOWN:
-        //        savedMatrix.set(matrix);
-        //        start.set(event.getX(), event.getY());
-        //        Log.d(TAG, "mode=DRAG");
-        //        mode = DRAG;
-        //        break;
-        //    case MotionEvent.ACTION_POINTER_DOWN:
-        //        oldDist = spacing(event);
-        //        Log.d(TAG, "oldDist=" + oldDist);
-        //        if (oldDist > 300f) {
-        //            midPoint(mid, event);
-        //            mode = ZOOM;
-        //            Log.d(TAG, "mode=ZOOM");
-        //        } else {
-        //            mode = DRAG;
-        //        }
-        //        break;
-        //    case MotionEvent.ACTION_UP:
-        //    case MotionEvent.ACTION_POINTER_UP:
-        //        mode = NONE;
-        //        Log.d(TAG, "mode=NONE");
-        //        break;
-        //    case MotionEvent.ACTION_MOVE:
-        //        if (mode == DRAG) {
-        //            // ...
-        //            matrix.set(savedMatrix);
-        //            matrix.postTranslate(event.getX() - start.x, event.getY()
-        //                    - start.y);
-        //        } else if (mode == ZOOM) {
-        //            float newDist = spacing(event);
-        //            Log.d(TAG, "newDist=" + newDist + " " + minDistance);
-        //            if (newDist > minDistance) {
-        //                matrix.set(savedMatrix);
-        //                float scale = newDist / oldDist;
-        //                matrix.postScale(scale, scale, mid.x, mid.y);
-        //                matrix.postTranslate(event.getX() - start.x, event.getY()
-        //                        - start.y);
-        //            } else {
-        //                matrix.set(savedMatrix);
-        //                matrix.postTranslate(event.getX() - start.x, event.getY()
-        //                        - start.y);
-        //            }
-        //        }
-        //        break;
-        //}
-        //
-        //setImageMatrix(matrix);
-        //return true;
+        if (getDrawable() == null) {
+            return false;
+        }
+        int action = event.getAction() & MotionEvent.ACTION_MASK;
+        mScaleDetector.onTouchEvent(event);
+        if (mScaleDetector.isInProgress()) {
+            start.set(event.getX(), event.getY());
+            return true;
+        }
+        switch (action) {
+            case MotionEvent.ACTION_MOVE:
+                matrix.postTranslate(event.getX() - start.x, event.getY() - start.y);
+                start.set(event.getX(), event.getY());
+                tempRect.set(0, 0, getDrawable().getIntrinsicWidth(), getDrawable().getIntrinsicHeight());
+                matrix.mapRect(tempRect);
+                float xShift = 0, yShift = 0;
+                if (tempRect.left > 0) {
+                    xShift = -tempRect.left;
+                } else if (tempRect.right < getWidth()) {
+                    xShift = getWidth() - tempRect.right;
+                }
+                if (tempRect.top > 0) {
+                    yShift = -tempRect.top;
+                } else if (tempRect.bottom < getHeight()) {
+                    yShift = getHeight() - tempRect.bottom;
+                }
+                matrix.postTranslate(xShift, yShift);
+                setImageMatrix(matrix);
+                imageRect.set(0, 0, getDrawable().getIntrinsicWidth(), getDrawable().getIntrinsicHeight());
+                matrix.mapRect(imageRect);
+                break;
+            default:
+                start.set(event.getX(), event.getY());
+                break;
+        }
+        return true;
     }
 
     private void dumpEvent(MotionEvent event) {
@@ -158,32 +121,6 @@ public class TouchImageViewSample extends ImageView {
         Log.d(TAG, sb.toString());
     }
 
-    /** Determine the space between the first two fingers */
-    private float spacing(MotionEvent event) {
-        float x = event.getX(0) - event.getX(1);
-        float y = event.getY(0) - event.getY(1);
-        return (float) Math.sqrt(x * x + y * y);
-    }
-
-    /** Calculate the mid point of the first two fingers */
-    private void midPoint(PointF point, MotionEvent event) {
-        float x = event.getX(0) + event.getX(1);
-        float y = event.getY(0) + event.getY(1);
-        point.set(x / 2, y / 2);
-    }
-
-    @Override
-    public void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-    }
-
-    /*
-     * (non-Javadoc)
-     *
-     * @see
-     * android.widget.ImageView#setImageDrawable(android.graphics.drawable.Drawable
-     * )
-     */
     @Override
     public void setImageDrawable(Drawable drawable) {
         // Constrain to given size but keep aspect ratio
@@ -203,12 +140,8 @@ public class TouchImageViewSample extends ImageView {
         }
         int width = drawable.getIntrinsicWidth();
         int height = drawable.getIntrinsicHeight();
-        mLastTouchX = mPosX = 0;
-        mLastTouchY = mPosY = 0;
 
         mScaleFactor = Math.max(w / (float) width, h / (float) height);
-        pivotPointX = (2 * w - width * mScaleFactor) / 2;
-        pivotPointY = (2 * h - height * mScaleFactor) / 2;
         float centerViewX = w / 2;
         float centerViewY = h / 2;
         float centerImageX = width / 2;
@@ -222,7 +155,6 @@ public class TouchImageViewSample extends ImageView {
         imageRect.set(0, 0, width, height);
         matrix.mapRect(imageRect);
         mMinScale = mScaleFactor;
-        Log.d(TAG, "Rect: " + imageRect.toShortString());
     }
 
     private class ScaleListener extends
@@ -271,15 +203,11 @@ public class TouchImageViewSample extends ImageView {
             setImageMatrix(matrix);
             imageRect.set(0, 0, width, height);
             matrix.mapRect(imageRect);
-            Log.d(TAG, imageRect.toShortString());
             return true;
         }
 
         @Override
         public boolean onScaleBegin(ScaleGestureDetector detector) {
-            if (getDrawable() == null) {
-                return false;
-            }
             anchorPoint[0] = detector.getFocusX();
             anchorPoint[1] = detector.getFocusY();
             pivotPoint[0] = anchorPoint[0];
